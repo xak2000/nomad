2bebf96462147029b15c4c12cd36147f2cf2d85c
Lang Martin <lang@hashicorp.com>
2019-11-06 16:52:12 -0500

    csi_volume_endpoint_test: failing on acl check

--- nomad/csi_volume_endpoint_test.go
+++ nomad/csi_volume_endpoint_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	msgpackrpc "github.com/hashicorp/net-rpc-msgpackrpc"
+	"github.com/hashicorp/nomad/acl"
+	"github.com/hashicorp/nomad/nomad/mock"
 	"github.com/hashicorp/nomad/nomad/structs"
 	"github.com/hashicorp/nomad/testutil"
 	"github.com/stretchr/testify/require"
@@ -15,6 +17,14 @@ func TestCSIVolumeEndpoint_Get(t *testing.T) {
 		c.NumSchedulers = 0 // Prevent automatic dequeue
 	})
 	defer s1.Shutdown()
+
+	state := s1.fsm.State()
+
+	validToken := mock.CreatePolicyAndToken(t, state, 1001, "test-valid",
+		mock.NamespacePolicy(structs.DefaultNamespace, "", []string{acl.NamespaceCapabilityCSIAccess}))
+	// invalidToken := mock.CreatePolicyAndToken(t, state, 1003, "test-invalid",
+	// 	mock.NamespacePolicy(structs.DefaultNamespace, "", []string{acl.NamespaceCapabilityCSIAccess}))
+
 	codec := rpcClient(t, s1)
 	testutil.WaitForLeader(t, s1.RPC)
 
@@ -26,21 +36,22 @@ func TestCSIVolumeEndpoint_Get(t *testing.T) {
 		ModeWriteOne: false,
 		ModeReadMany: true,
 	}}
-	err := s1.fsm.State().CSIVolumeRegister(0, vols)
+	err := state.CSIVolumeRegister(0, vols)
 	require.NoError(t, err)
 
 	// Create the register request
 	req := &structs.CSIVolumeSingleRequest{
 		ID: "DEADBEEF-70AD-4672-9178-802BCA500C87",
+		QueryOptions: structs.QueryOptions{
+			Region:    "global",
+			AuthToken: validToken.SecretID,
+		},
 	}
 
 	var resp structs.CSIVolumeSingleResponse
-	if err := msgpackrpc.CallWithCodec(codec, "CSIVolume.GetVolume", req, &resp); err != nil {
-		t.Fatalf("err: %v", err)
-	}
-	if resp.Index == 0 {
-		t.Fatalf("bad index: %d", resp.Index)
-	}
+	err = msgpackrpc.CallWithCodec(codec, "CSIVolume.GetVolume", req, &resp)
+	require.NoError(t, err)
+	require.NotEqual(t, 0, resp.Index)
 
 	// MLM check props
 }
